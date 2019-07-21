b1ba01d5a1eda3c321c0b49a609d510c3a62dd1a
eviatar21-meet <eviatar21@meet.mit.edu>
2019-07-21 18:26:24 +0300

    Stopping here

---
+++ EVIATARinTurtle.py
@@ -0,0 +1,38 @@
+import turtle
+
+turtle.penup() 
+turtle.goto(-200,-100) 
+turtle.pendown()
+
+#E
+turtle.goto(-200, -100)
+turtle.pendown()
+turtle.goto(-200,100)
+turtle.goto(-100,100)
+turtle.penup()
+turtle.goto(-200,0)
+turtle.pendown()
+turtle.goto(-100,0)
+turtle.penup()
+turtle.goto(-200,-100)
+turtle.pendown()
+turtle.goto(-100,-100)
+
+
+turtle.penup()
+#V
+turtle.goto(0, -100)
+turtle.pendown()
+turtle.goto(-50, 100)
+turtle.penup()
+turtle.goto(0, -100)
+turtle.pendown()
+turtle.goto(50, 100)
+turtle.penup()
+#I
+turtle.goto(100,-100)
+turtle.pendown()
+turtle.goto(100,100)
+
+turtle.mainloop()
+
